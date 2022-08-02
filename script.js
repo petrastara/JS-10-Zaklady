c580c80481659aa8940fb636d3c57555d5f91f3c
Petra Stará <petrastara1@seznam.cz>
2022-08-02 09:20:59 +0200

    výpočet věku

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("JavaScript funguje!");
+
+console.log( Math.floor(Math.random()*100));
+
+/*let mojeBarvaVlasu = prompt ("zadej svoji barvu vlasů");*/
+
+/*zeptám se na rok narození -> kolik je let*/
+
+let rok = prompt ("zadejte rok svého narození");
+let dnesniDatum = new Date();
+let stari = dnesniDatum.getFullYear() - rok;     /*vyměnit akt. datum tak, aby se měnilo samo (rok) → vygooglit*/
+console.log ("uživatel má "+ stari + " let");
